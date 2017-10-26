27711d7983589d2429d96d9eeb32cd07dcba6df2
zhongliu <liuzhongliu93@gmail.com>
2017-10-26 15:24:20 +0800

    update

--- chapter10/slideshow/scripts/prepareSlideshow.js
+++ chapter10/slideshow/scripts/prepareSlideshow.js
@@ -1,27 +1,31 @@
 function prepareSlideshow() {
-// Make sure the browser understands the DOM methods
-  if (!document.getElementsByTagName) return false;
-  if (!document.getElementById) return false;
-// Make sure the elements exist
-  if (!document.getElementById("linklist")) return false;
-  if (!document.getElementById("preview")) return false;
-// Apply styles to the preview image
-  var preview = document.getElementById("preview");
-  preview.style.position = "absolute";
-  preview.style.left = "0px";
-  preview.style.top = "0px";
-// Get all the links in the list
-  var list = document.getElementById("linklist");
-  var links = list.getElementsByTagName("a");
-// Attach the animation behavior to the mouseover event
-  links[0].onmouseover = function() {
-    moveElement("preview",-100,0,10);
-  }
-  links[1].onmouseover = function() {
-    moveElement("preview",-200,0,10);
-  }
-  links[2].onmouseover = function() {
-    moveElement("preview",-300,0,10);
-  }
+	//确保浏览器支持DOM方法
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById) return false;
+	//确保元素存在
+	if(!document.getElementById("linklist")) return false;
+	//添加div
+	var slideshow = document.createElement("div");
+	slideshow.setAttribute("id", "slideshow");
+	//创建img元素
+	var preview = document.createElement("img")
+	preview.setAttribute("src", "images/topics.gif")
+	preview.setAttribute("alt", "building blocks of web design");
+	preview.setAttribute("id", "preview");
+	slideshow.appendChild(preview);
+	var list = document.getElementById("linklist");
+	insertAfter(slideshow,list);
+	//取得列表中的所有链接
+	var links = list.getElementsByTagName("a");
+	//为mouseover事件添加动画效果
+	links[0].onmouseover = function() {
+		moveElement("preview",-100,0,10);
+	}
+	links[1].onmouseover = function() {
+		moveElement("preview",-200,0,10);
+	}
+	links[2].onmouseover = function() {
+		moveElement("preview",-300,0,10);
+	}
 }
 addLoadEvent(prepareSlideshow);
